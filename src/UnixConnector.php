42f269bc26e9533eb9c0cb884e7253ed20da62d9
Christian Lück <christian@lueck.tv>
2014-11-27 00:59:46 +0100

    Workaround connection rejection race condition See https://github.com/clue/php-buzz-react/issues/19

--- src/UnixConnector.php
+++ src/UnixConnector.php
@@ -16,6 +16,9 @@ use RuntimeException;
  *
  * Unix domain sockets use atomic operations, so we can as well emulate
  * async behavior.
+ *
+ * Use a small delay of 0.001s to avoid a race condition in the http library,
+ * see https://github.com/clue/php-buzz-react/issues/19
  */
 class UnixConnector implements ConnectorInterface
 {
@@ -32,13 +35,17 @@ class UnixConnector implements ConnectorInterface
     {
         $deferred = new Deferred();
 
-        $resource = @stream_socket_client($this->path, $errno, $errstr, 1.0);
+        $path = $this->path;
+        $loop = $this->loop;
+        $this->loop->addTimer(0.001, function() use ($deferred, $path, $loop) {
+            $resource = @stream_socket_client($this->path, $errno, $errstr, 1.0);
 
-        if (!$resource) {
-            $deferred->reject(new RuntimeException('Unable to connect to unix domain socket path: ' . $errstr, $errno));
-        } else {
-            $deferred->resolve(new Stream($resource, $this->loop));
-        }
+            if (!$resource) {
+                $deferred->reject(new RuntimeException('Unable to connect to unix domain socket path: ' . $errstr, $errno));
+            } else {
+                $deferred->resolve(new Stream($resource, $this->loop));
+            }
+        });
 
         return $deferred->promise();
     }
