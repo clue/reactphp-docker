d9420c43bbe43754b49d789317fc519cc659c689
Christian Lück <christian@lueck.tv>
2014-11-27 12:43:34 +0100

    Test interaction between Client and ResponseParser

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -8,109 +8,122 @@ use React\Promise\Deferred;
 class ClientTest extends TestCase
 {
     private $browser;
+    private $parser;
     private $client;
 
     public function setUp()
     {
         $this->browser = $this->getMockBuilder('Clue\React\Buzz\Browser')->disableOriginalConstructor()->getMock();
-        $this->client = new Client($this->browser);
+        $this->parser = $this->getMock('Clue\React\Docker\Io\ResponseParser');
+        $this->client = new Client($this->browser, $this->parser);
     }
 
     public function testPing()
     {
-        $this->browser->expects($this->once())->method('get')->with($this->equalTo('/_ping'))->will($this->returnResponse('OK'));
+        $this->expectRequestFlow('get', '/_ping', $this->createResponse('OK'), 'expectPlain');
 
         $this->client->ping();
     }
 
     public function testContainerInspect()
     {
-        $this->browser->expects($this->once())->method('get')->with($this->equalTo('/containers/123/json'))->will($this->returnResponse('{}'));
+        $this->expectRequestFlow('get', '/containers/123/json', $this->createResponse('{}'), 'expectJson');
 
         $this->client->containerInspect(123);
     }
 
     public function testContainerTop()
     {
-        $this->browser->expects($this->once())->method('get')->with($this->equalTo('/containers/123/top'))->will($this->returnResponse('{}'));
+        $this->expectRequestFlow('get', '/containers/123/top', $this->createResponse('{}'), 'expectJson');
 
         $this->client->containerTop(123);
     }
 
     public function testContainerWait()
     {
-        $this->browser->expects($this->once())->method('post')->with($this->equalTo('/containers/123/wait'))->will($this->returnResponse('{}'));
+        $this->expectRequestFlow('post', '/containers/123/wait', $this->createResponse('{}'), 'expectJson');
 
         $this->client->containerWait(123);
     }
 
     public function testContainerKill()
     {
-        $this->browser->expects($this->once())->method('post')->with($this->equalTo('/containers/123/kill?signal='))->will($this->returnResponse());
+        $this->expectRequestFlow('post', '/containers/123/kill?signal=', $this->createResponse(), 'expectEmpty');
 
         $this->client->containerKill(123);
     }
 
     public function testContainerKillSignalName()
     {
-        $this->browser->expects($this->once())->method('post')->with($this->equalTo('/containers/123/kill?signal=SIGKILL'))->will($this->returnResponse());
+        $this->expectRequestFlow('post', '/containers/123/kill?signal=SIGKILL', $this->createResponse(), 'expectEmpty');
 
         $this->client->containerKill(123, 'SIGKILL');
     }
 
     public function testContainerKillSignalNumber()
     {
-        $this->browser->expects($this->once())->method('post')->with($this->equalTo('/containers/123/kill?signal=9'))->will($this->returnResponse());
+        $this->expectRequestFlow('post', '/containers/123/kill?signal=9', $this->createResponse(), 'expectEmpty');
 
         $this->client->containerKill(123, 9);
     }
 
     public function testContainerStop()
     {
-        $this->browser->expects($this->once())->method('post')->with($this->equalTo('/containers/123/stop?t=10'))->will($this->returnResponse());
+        $this->expectRequestFlow('post', '/containers/123/stop?t=10', $this->createResponse(), 'expectEmpty');
 
         $this->client->containerStop(123, 10);
     }
 
     public function testContainerRestart()
     {
-        $this->browser->expects($this->once())->method('post')->with($this->equalTo('/containers/123/restart?t=10'))->will($this->returnResponse());
+        $this->expectRequestFlow('post', '/containers/123/restart?t=10', $this->createResponse(), 'expectEmpty');
 
         $this->client->containerRestart(123, 10);
     }
 
     public function testContainerPause()
     {
-        $this->browser->expects($this->once())->method('post')->with($this->equalTo('/containers/123/pause'))->will($this->returnResponse());
+        $this->expectRequestFlow('post', '/containers/123/pause', $this->createResponse(), 'expectEmpty');
 
         $this->client->containerPause(123);
     }
 
     public function testContainerUnpause()
     {
-        $this->browser->expects($this->once())->method('post')->with($this->equalTo('/containers/123/unpause'))->will($this->returnResponse());
+        $this->expectRequestFlow('post', '/containers/123/unpause', $this->createResponse(), 'expectEmpty');
 
         $this->client->containerUnpause(123);
     }
 
     public function testContainerDelete()
     {
-        $this->browser->expects($this->once())->method('delete')->with($this->equalTo('/containers/123?v=0&force=0'))->will($this->returnResponse());
+        $this->expectRequestFlow('delete', '/containers/123?v=0&force=0', $this->createResponse(), 'expectEmpty');
 
         $this->client->containerDelete(123, false, false);
     }
 
     public function testContainerResize()
     {
-        $this->browser->expects($this->once())->method('get')->with($this->equalTo('/containers/123/resize?w=800&h=600'))->will($this->returnResponse());
+        $this->expectRequestFlow('get', '/containers/123/resize?w=800&h=600', $this->createResponse(), 'expectEmpty');
 
         $this->client->containerResize(123, 800, 600);
     }
 
-    private function returnResponse($body = '')
+    private function expectRequestFlow($method, $url, Response $response, $parser)
+    {
+        $this->browser->expects($this->once())->method($method)->with($this->equalTo($url))->will($this->returnPromise($response));
+        $this->parser->expects($this->once())->method($parser)->with($this->equalTo($response));
+    }
+
+    private function createResponse($body = '')
+    {
+        return new Response('HTTP/1.0', 200, 'OK', null, new Body($body));
+    }
+
+    private function returnPromise($for)
     {
         $deferred = new Deferred();
-        $deferred->resolve(new Response('HTTP/1.0', 200, 'OK', null, new Body($body)));
+        $deferred->resolve($for);
 
         return $this->returnValue($deferred->promise());
     }
