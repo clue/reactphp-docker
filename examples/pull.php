fca44a4594d6b57de0b54cfe6e1c7d01ee93bcc7
Christian Lück <christian@lueck.tv>
2015-06-15 23:33:42 +0200

    Rename pull example

--- examples/pull.php
+++ examples/pull.php
@@ -6,6 +6,7 @@ use React\EventLoop\Factory as LoopFactory;
 use Clue\React\Docker\Factory;
 
 $image = isset($argv[1]) ? $argv[1] : 'clue/redis-benchmark';
+echo 'Pulling image "' . $image . '" (pass as argument to this example)' . PHP_EOL;
 
 $loop = LoopFactory::create();
 
@@ -14,7 +15,7 @@ $client = $factory->createClient();
 
 $client->imageCreate($image)->then(
     function ($response) {
-        echo 'response: '; var_dump($response);
+        echo 'response: ' . json_encode($response) . PHP_EOL;
     },
     'var_dump',
     function ($info) {
