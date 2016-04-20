950f8cdf0788d2dd82c583e3eead79a27e1e12fd
Christian Lück <christian@lueck.tv>
2016-04-21 00:19:18 +0200

    Documentation for public APIs

--- src/Factory.php
+++ src/Factory.php
@@ -5,14 +5,25 @@ namespace Clue\React\Docker;
 use React\EventLoop\LoopInterface;
 use Clue\React\Buzz\Browser;
 use Clue\React\Buzz\Io\Sender;
-use React\HttpClient\Client as HttpClient;
-use Clue\React\Docker\Io\UnixConnector;
 
+/**
+ * The Factory is responsible for creating your Client instance.
+ * It also registers everything with the main EventLoop.
+ */
 class Factory
 {
     private $loop;
     private $browser;
 
+    /**
+     * Instantiate the Factory
+     *
+     * If you need custom DNS, SSL/TLS or proxy settings, you can explicitly
+     * pass a custom Browser instance.
+     *
+     * @param LoopInterface $loop    the event loop
+     * @param null|Browser  $browser (optional) custom Browser instance to use
+     */
     public function __construct(LoopInterface $loop, Browser $browser = null)
     {
         if ($browser === null) {
@@ -23,6 +34,12 @@ class Factory
         $this->browser = $browser;
     }
 
+    /**
+     * Creates a new Client instance and helps with constructing the right Browser object for the given remote URL
+     *
+     * @param null|string $url (optional) URL to your (local) Docker daemon, defaults to using local unix domain socket path
+     * @return Client
+     */
     public function createClient($url = null)
     {
         if ($url === null) {
@@ -33,8 +50,9 @@ class Factory
 
         if (substr($url, 0, 7) === 'unix://') {
             // send everything through a local unix domain socket
-            $sender = Sender::createFromLoopUnix($this->loop, $url);
-            $browser = $browser->withSender($sender);
+            $browser = $this->browser->withSender(
+                Sender::createFromLoopUnix($this->loop, $url)
+            );
 
             // pretend all HTTP URLs to be on localhost
             $url = 'http://localhost/';
